3a5505835ae57ca7962ffeef72c55a8881968673
Spencer Gerdes <skgerdes@yahoo.com>
2018-08-16 19:52:17 -0500

    Refactor of the show/hide nav func

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -5,23 +5,31 @@ window.addEventListener("load",function(){
 	var navActive = false;
 
 	$(window).resize(adjustNavOnResize);
-	$(HAMBURGER).click(shownav);
+	$(HAMBURGER).click(toggleNav);
+
+	function toggleNav(){
+		event.preventDefault();
+		if (!navActive){
+			showNav();
+		}
+		else{
+			hideNav();
+		};
+	};
+
+	function showNav(){
+		NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";
+		navActive = true;
+	};
+
+	function hideNav(){
+		NAVBAR.style.top = "-10px";
+		navActive = false;
+	};
 
 	function adjustNavOnResize(){
 		if (navActive){
 			NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";	
-		}
-	}
-
-	function shownav(){
-		event.preventDefault();
-		if (!navActive){
-			NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";
-			navActive = true;
-		}
-		else{
-			NAVBAR.style.top = "-10px";
-			navActive = false;
 		};
 	};
 });
