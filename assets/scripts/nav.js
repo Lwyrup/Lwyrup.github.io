547f050e7f9bf5ffabc00029c024c03b8fb70ff9
Spencer Gerdes <skgerdes@yahoo.com>
2018-08-16 20:42:12 -0500

    Complete refactor leveraging jquery methods

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -2,34 +2,16 @@ window.addEventListener("load",function(){
 
 	const HAMBURGER = $(".headerInfo__hamburger")[0];
 	const NAVBAR = $(".dropNav")[0];
-	var navActive = false;
 
 	$(window).resize(adjustNavOnResize);
 	$(HAMBURGER).click(toggleNav);
 
 	function toggleNav(){
 		event.preventDefault();
-		if (!navActive){
-			showNav();
-		}
-		else{
-			hideNav();
-		};
+		$(NAVBAR).animate({top: 'toggle'}, 300);
 	};
 
-	function showNav(){
-		NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";
-		navActive = true;
-	};
-
-	function hideNav(){
-		NAVBAR.style.top = "-10px";
-		navActive = false;
-	};
-
-	function adjustNavOnResize(){
-		if (navActive){
-			NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";	
-		};
+	function adjustNavOnResize(){	
+		NAVBAR.style.top = $(".headerBg")[0].clientHeight + "px";	
 	};
 });
